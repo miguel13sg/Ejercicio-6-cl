ff10d9a965809756bdd063cd16871660e5626a9b
miguel13sg <saj252149@uvg.edu.gt>
2025-10-31 18:53:33 -0600

    Refactor estaciones class with comments
    Added comments to clarify class methods and structure.

--- estaciones.java
+++ estaciones.java
@@ -1,21 +1,25 @@
 public class estaciones extends Equipos implements Medible, Registrable{
     private String[] parametros;
     
+    // Constructor de la clase estaciones
     public estaciones(int ID, String nombre, int consumo, String[] parametros) {
         super(ID, nombre, consumo);
         this.parametros = parametros;
     }
 
+    // Implementacion del metodo de la interfaz Medible
     @Override
     public String medir() {
         return "Estación " + nombre + " midiendo: " + String.join(", ", parametros);
     }
 
+    // Implementacion del metodo de la interfaz Registrable
     @Override
     public String registrar() {
         return "Estación " + nombre + " registrando datos meteorológicos";
     }
     
+    // Sobrescribe el metodo getInfo para incluir informacion especifica de estaciones
     @Override
     public String getInfo() {
         return super.getInfo() + ", Parámetros: " + String.join(", ", parametros);
