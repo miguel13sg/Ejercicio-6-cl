3705d5be5f7bf2c5822b9e859a3aa80260550c7e
miguel13sg <saj252149@uvg.edu.gt>
2025-10-31 18:52:55 -0600

    Add comments to drones class methods
    Added comments to the drones class for clarity.

--- drones.java
+++ drones.java
@@ -1,19 +1,26 @@
 public class drones extends Equipos implements Accionable, Registrable{
     private String accion;
     
+    // Constructor de la clase drones
     public drones(int ID, String nombre, int consumo, String accion)
     {
         super(ID, nombre, consumo);
         this.accion = accion;
     }
+    
+    // Implementacion del metodo de la interfaz Accionable
     @Override
     public String ejecutarAccion() {
         return "Dron " + nombre + " ejecutando: " + accion;
     }
+    
+    // Implementacion del metodo de la interfaz Registrable
     @Override
     public String registrar() {
         return "Dron " + nombre + " registrando imágenes multiespectrales";
     }
+    
+    // Sobrescribe el metodo getInfo para incluir informacion especifica de drones
     @Override
     public String getInfo() {
         return super.getInfo() + ", Acción: " + accion;
