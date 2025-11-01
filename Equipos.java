9bbdd2b0fd79dc70acbe57e4be63829c763f17e9
miguel13sg <saj252149@uvg.edu.gt>
2025-10-31 18:50:01 -0600

    Refactor Equipos class with comments and formatting

--- Equipos.java
+++ Equipos.java
@@ -3,15 +3,20 @@ public abstract class Equipos{
     protected String nombre;
     protected int consumo;   
 
+    // Constructor de la clase base Equipos
     public Equipos(int ID, String nombre, int consumo)
     {
         this.nombre = nombre;
         this.consumo = consumo;
         this.ID = ID;
     }
+    
+    // Devuelve informacion basica del equipo
     public String getInfo() {
         return "ID: " + ID + ", Nombre: " + nombre + ", Consumo: " + consumo + "W";
     }
+    
+    // Overloading: Devuelve informacion basica o detallada segun el parametro
     public String getInfo(boolean detallada) {
         if (detallada) {
             return getInfo() + ", Tipo: " + this.getClass().getSimpleName();
