2f8275d93546ec3a760c071001ba5cd2764df4ef
miguel13sg <saj252149@uvg.edu.gt>
2025-10-31 18:55:50 -0600

    Add comments to sensores class methods
    Added comments to clarify class methods and constructor.

--- sensores.java
+++ sensores.java
@@ -1,16 +1,19 @@
 public class sensores extends Equipos implements Medible{
     private String medicion;
 
+    // Constructor de la clase sensores
     public sensores(int ID, String nombre, int consumo, String medicion) {
         super(ID, nombre, consumo);
         this.medicion = medicion;
     }
 
+    // Implementacion del metodo de la interfaz Medible
     @Override
     public String medir() {
         return "Sensor " + nombre + " midiendo: " + medicion;
     }
 
+    // Sobrescribe el metodo getInfo para incluir informacion especifica de sensores
     @Override
     public String getInfo() {
         return super.getInfo() + ", Medición: " + medicion;
